16398e3b813b04f91301773cdfcd480a16e814ed
TK <kotadiyasumita@gmail.com>
2026-03-10 12:18:46 +0530

    Update sw.js
    Signed-off-by: TK <kotadiyasumita@gmail.com>

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "studyloop-cache-v9"; // <-- changed v1 -> v2
+const CACHE = "studyloop-cache-v300"; // <-- changed v1 -> v2
 const CORE = [
   "./",
   "./index.html",
@@ -49,6 +49,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
